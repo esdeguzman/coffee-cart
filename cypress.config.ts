250b6df0439c5cc194a2af36594ce70d4753d7bc
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2026-03-16 12:00:15 +0800

    Update: add rename configuration to cypress report

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
     video: true,
     reporter: "mochawesome",
     reporterOptions: {
-      reportDir: "cypress/reports"
+      reportDir: "cypress/reports",
+      reportFilename: "[status]_[datetime]-[name]-report",
+      timestamp: "longDate"
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
